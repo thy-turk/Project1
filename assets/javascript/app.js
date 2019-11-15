a4ccd2b96a08373c475934d50d07cb14e0b84a71
Tucker Fiumara <54918455+thy-turk@users.noreply.github.com>
2019-11-15 14:39:27 -0700

    Added list of resort IDs for widget and temporary widget stuff in the index.html (#5)

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var resortId = ["20", "25", "36", "77", "113", "120", "1673", "143", "181", "197", "220", "240", "329", 
+"330", "1435", "372", "425", "445", "456", "482", "507", "511"]
+
+
+/* Arapahoe Basin 20, Aspen Snowmass 25, Aspen Highlands 25, Aspen Mountain 25, Beaver Creek 36, Breckenridge 77, Buttermilk 25, 
+Copper 113, Crested Butte 120, echo mountain 1673, Eldora 143, GRANBY NO RESULT, Howelson 181, Keystone 197, Loveland 220, 
+Monarch Mtn 240, PowderHorn 329, Purgatory 330, Silverton 1435, Ski Cooper 372, Steamboat 425, Sunlight Mtn 445, Telluride 456, 
+Vail 482, Winter Park 507, Wolf Creek 511,
+*/
+
