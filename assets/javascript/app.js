340bf22a8d257172f97dd24b5b50f8853aeea0fc
LucasAho <aholucas11@gmail.com>
2019-11-16 13:05:59 -0700

    New lucas (#10)
    * Weather test logged
    
    * Weather test logged
    
    * Temperature logged for midday one day away
    
    * Weather descriptor added
    
    * Psuedo
    
    * Widget added
    
    * Widget added
    
    * Widget added again?
    
    * HTML fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,7 @@ var weather;
 
 var resortZip = 80435; //change to button value with mike drop down
 $("#test").on("click", function() {
+    $(".widget1Display") = "<div class='ots-widget'><iframe id='o28858' width='600' height='150' border='0' frameborder='0' scrolling='no' src='https://www.onthesnow.com/widget/list?resorts=" + resortId[4] + "&color=b'></iframe><p><a href='https://www.onthesnow.com' target='_blank'>Powered by OnTheSnow.com</a></p></div>";
     
     var openWeatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + resortZip + ",us&units=imperial&APPID=8945840c8250c919b4821b938074f3a6"
 
@@ -27,6 +28,8 @@ $("#test").on("click", function() {
         weather = weatherArray[5].weather[0].description;
     })
 }) 
+
+
 
 /* Arapahoe Basin 20, Aspen Snowmass 25, Aspen Highlands 25, Aspen Mountain 25, Beaver Creek 36, Breckenridge 77, Buttermilk 25, 
 Copper 113, Crested Butte 120, echo mountain 1673, Eldora 143, GRANBY NO RESULT, Howelson 181, Keystone 197, Loveland 220, 
